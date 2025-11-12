3b8ca2751317e56dfbcafda4620fe3ef4185fded
charlainebautista <chna.bautista.up@phinmaed.com>
2025-11-12 15:06:09 +0800

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Homepage from './page/Homepage.tsx'
-import Projects from './page/Projects.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./page/Homepage.tsx";
+import Projects from "./page/Projects.tsx";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const router = createBrowserRouter([
-{path:"/",element:<Homepage/>},
-{path:"/projects",element:<Projects/>},
-
-
+  { path: "/", element: <Homepage /> },
+  { path: "/projects", element: <Projects /> },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <Analytics />
+    <SpeedInsights />
+    <RouterProvider router={router} />
+  </StrictMode>
+);
