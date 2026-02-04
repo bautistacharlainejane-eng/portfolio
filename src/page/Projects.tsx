d2af17aac28d23f93a5a397819d9554d90122068
charlainebautista <chna.bautista.up@phinmaed.com>
2026-02-04 16:46:00 +0800

    project

--- src/page/Projects.tsx
+++ src/page/Projects.tsx
@@ -1,112 +1,88 @@
 import Header from "../components/Header";
 
-
 import webDesignProject from "../assets/web-design-project.png";
-import webDesignProject1 from "../assets/web-design-project1.png";
 
 import mobDesignProject from "../assets/mobile-design-project.png";
-import graphicDesigns from "../assets/graphic-designs.png";
-
-import arrowRight from "../assets/arrow-right.png";
 
 export default function () {
   return (
     <>
-          
       <main className="font-poppins bg-[#0D1210]  ">
         <Header></Header>
 
         <section className=" py-6 mb-6  ">
-          <div className="">
+          <div className="px-12">
             <h1 className="text-3xl font-semibold flex justify-center text-white">
               Projects
             </h1>
             {/* Project 1 */}
 
-            <div className="grid grid-cols-1 md:grid-cols-3 mt-12">
-              <div className="group p-4 bg-[#182B18]/60 backdrop-blur-md text-[#0D1210]">
+            <div className="grid grid-cols-1 md:grid-cols-2 mt-12">
+              <div className="p-4 flex flex-col space-y-8 px-8 py-16 text-white">
+                <p className="text-2xl font-semibold">Website Design</p>
+                <p className="text-md font-normal">
+                  By focusing on user needs, visual consistency, and usability.
+                  Start with user research and planning, then design wireframes
+                  and layouts that are both functional and visually appealing.
+                  Ensuring the design is responsive, and aligned with the
+                  brand’s goals, creating a seamless experience across different
+                  devices.
+                </p>
+                <ul className="space-y-2">
+                  <li className="relative pl-6 before:content-['✔'] before:absolute before:left-0 before:text-green-500 before:font-bold">
+                    Wireframes
+                  </li>
+                  <li className="relative pl-6 before:content-['✔'] before:absolute before:left-0 before:text-green-500 before:font-bold">
+                    Mock-ups
+                  </li>
+                  <li className="relative pl-6 before:content-['✔'] before:absolute before:left-0 before:text-green-500 before:font-bold">
+                    Prototypes
+                  </li>
+                </ul>
+              </div>
+
+              <div className="group p-4 text-[#0D1210]">
                 <img
                   className="w-auto object-contain"
                   src={webDesignProject}
                   alt="UI/UX Icon"
                 />
               </div>
-
-              <div className="p-4 bg-[#182B18]/60 backdrop-blur-md flex flex-col justify-between space-y-8 px-8 py-16 text-white">
-                <p className="text-lg font-semibold">Web Design</p>
-                <p className="text-xs font-normal">
-                  A modern, user-friendly web designs that are visually
-                  appealing, responsive, and easy to navigate.
-                </p>
-
-                <div className="flex justify-end">
-                  <button className="group">
-                    <img
-                      className="w-[2rem] object-contain transition-transform duration-300 ease-in-out group-hover:translate-x-1 group-hover:opacity-80 cursor-pointer"
-                      src={arrowRight}
-                      alt="UI/UX Icon"
-                    />
-                  </button>
-                </div>
-              </div>
-
-              <div className="group p-4 bg-[#182B18]/60 backdrop-blur-md text-[#0D1210]">
-                <img
-                  className=" w-auto object-contain"
-                  src={webDesignProject1}
-                  alt="UI/UX Icon"
-                />
-              </div>
             </div>
             {/* End of Project 1 */}
 
-            {/* Project2 */}
+            {/* Project 1 */}
 
-            <div className="grid grid-cols-1 md:grid-cols-3">
-              <div className="bg-[#182B18]/60 backdrop-blur-md flex flex-col justify-between space-y-8 px-8 py-16 text-white">
-                <p className="text-lg font-semibold">Mobile Design</p>
-                <p className="text-xs font-normal">
-                  A modern, user-friendly web designs that are visually
-                  appealing, responsive, and easy to navigate.
-                </p>
-
-                <div className="flex justify-end">
-                  <button className="group">
-                    <img
-                      className="w-[2rem] object-contain transition-transform duration-300 ease-in-out group-hover:translate-x-1 group-hover:opacity-80 cursor-pointer"
-                      src={arrowRight}
-                      alt="UI/UX Icon"
-                    />
-                  </button>
-                </div>
-              </div>
-
-              <div className="group p-4 bg-[#182B18]/60 backdrop-blur-md text-[#0D1210] rounded-t-l-xl">
+            <div className="grid grid-cols-1 md:grid-cols-2 mt-12">
+              <div className="group p-4 text-[#0D1210]">
                 <img
-                  className=" w-auto object-contain"
+                  className="w-auto object-contain"
                   src={mobDesignProject}
                   alt="UI/UX Icon"
                 />
               </div>
 
-              <div className="bg-[#182B18]/60 backdrop-blur-md flex flex-col justify-between space-y-8 px-8 py-16 text-white">
-                <p className="text-lg font-semibold">
-                  Prototyping & Wireframing
+              <div className="p-4 flex flex-col space-y-8 px-8 py-16 text-white">
+                <p className="text-2xl font-semibold">Mobile Design</p>
+                <p className="text-md font-normal">
+                  Creates mobile designs by focusing on simplicity, usability,
+                  and accessibility.Ensuring smooth navigation, clear visual
+                  hierarchy, and responsive interactions. The process includes
+                  creating wireframes, mock-ups, and prototypes to deliver a
+                  consistent and user-friendly experience across different
+                  mobile devices.
                 </p>
-                <p className="text-xs font-normal">
-                  A modern, user-friendly web designs that are visually
-                  appealing, responsive, and easy to navigate.
-                </p>
-
-                <div className="flex justify-end">
-                  <button className="group">
-                    <img
-                      className="w-[2rem] object-contain transition-transform duration-300 ease-in-out group-hover:translate-x-1 group-hover:opacity-80 cursor-pointer"
-                      src={arrowRight}
-                      alt="UI/UX Icon"
-                    />
-                  </button>
-                </div>
+                <ul className="space-y-2">
+                  <li className="relative pl-6 before:content-['✔'] before:absolute before:left-0 before:text-green-500 before:font-bold">
+                    Wireframes
+                  </li>
+                  <li className="relative pl-6 before:content-['✔'] before:absolute before:left-0 before:text-green-500 before:font-bold">
+                    Mock-ups
+                  </li>
+                  <li className="relative pl-6 before:content-['✔'] before:absolute before:left-0 before:text-green-500 before:font-bold">
+                    Prototypes
+                  </li>
+                </ul>
               </div>
             </div>
             {/* End of Project 1 */}
